297c9aa4e38be2c3cd94727c61e7c0fbe4069433
shixin <shixin@xiaomi.com>
2014-03-18 09:21:23 +0800

    add sql

--- sql/allTables.sql
+++ sql/allTables.sql
@@ -1,1 +1,23 @@
-create table
+drop table if exists `user`;
+create table user(
+user_id int(10) not null primary key auto_increment,
+user_nickname varchar(64) not null,
+user_pwd varchar(64) not null,
+user_email varchar(64) not null,
+user_category varchar(64) not null,
+create_time timestamp not null default CURRENT_TIMESTAMP,
+update_time timestamp on update CURRENT_TIMESTAMP
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+drop table if exists `user_info`;
+create table user_info(
+user_id int(10) not null primary key,
+cellphone_number varchar(16),
+telephone_number varchar(32),
+country varchar(32),
+city varchar(32),
+address varchar(32),
+birthday date,
+create_time timestamp not null default CURRENT_TIMESTAMP,
+update_time timestamp on update CURRENT_TIMESTAMP
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
---
+++ src/main/java/com/chinalife/servlet/RegisterServlet.java
@@ -0,0 +1,14 @@
+package com.chinalife.servlet;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by ishikin on 14-3-18.
+ */
+public class RegisterServlet extends BaseServlet {
+    @Override
+    public void processRequest(HttpServletRequest request, HttpServletResponse response) {
+        
+    }
+}
